bbffcb515c408b6ee4cef12d5052af4fe14edaeb
fiza310 <fizanaeem310@gmail.com>
2024-11-08 17:26:39 +0500

    first commit

---
+++ app/components/header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header () {
+    return (
+        <div className="h-[134px] p-4" style={{backgroundColor: 'rgb(162,152,117) '}}>
+
+
+            <h1 className='text-3xl font-semibold text-white'>MANZZARI</h1>
+        
+        
+        
+        </div>
+    )
+}
+
+export default Header
